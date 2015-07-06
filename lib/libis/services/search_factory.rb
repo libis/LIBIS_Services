7f4c5c2f82070c92cad5c3a0116dfd40110896cf
Kris Dekeyser <kris.dekeyser@libis.be>
2015-07-06 18:26:36 +0200

    SearchFactory

--- lib/libis/services/search_factory.rb
+++ lib/libis/services/search_factory.rb
@@ -7,17 +7,34 @@ require_relative 'scope/search'
 
 module Libis
   module Services
+
     class SearchFactory
-      def initialize(format)
+      def initialize(format, *args)
         @search_class = self.class.const_get("Libis::Services::#{format}::Search")
+        @search_client = @search_class.new *args
+
       rescue Exception => e
         puts e.message
         exit -1
       end
 
-      def new_search(*args)
-        @search_class.new *args
+      def query(query, options = {})
+        @search_client.get(query, options)
       end
+
+      def find(term, options = {})
+        @search_client.find(term, options)
+      end
+
+      def each(options = {}, &block)
+        @search_client.each(options, &block)
+      end
+
+      def next_record(options = {}, &block)
+        @search_client.next_record(options, &block)
+      end
+
     end
+
   end
 end

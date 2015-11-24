e72e578353c75feb6ffce2d236c4188210c4494d
Kris Dekeyser <kris.dekeyser@libis.be>
2015-11-24 15:53:24 +0100

    Test update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,19 @@ Bundler.setup
 require 'rspec'
 require 'libis-services'
 
+def check_container(expected, result)
+  case expected
+    when Array
+      expect(result).to be_a Array
+      expected.each_with_index { |value, i| check_container(value, result[i]) }
+    when Hash
+      expect(result).to be_a Hash
+      expected.each_key do |key|
+        expect(result[key]).not_to be_nil
+        check_container(expected[key], result[key])
+      end
+    else
+      expect(result).to eq expected
+  end
+end
+

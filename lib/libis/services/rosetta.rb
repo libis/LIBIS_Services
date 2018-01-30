e811782b3ce696a7892e687bb62d3c68b03bd2ca
Kris Dekeyser <kris.dekeyser@libis.be>
2018-01-30 12:00:27 +0100

    Version bump: 0.1.16 - include fix

--- lib/libis/services/rosetta.rb
+++ lib/libis/services/rosetta.rb
@@ -1,15 +1,15 @@
 module Libis
   module Services
     module Rosetta
-      autoload :Service, 'rosetta/service'
+      autoload :Service, 'lib/libis/services/rosetta/service'
 
-      autoload :PdsHandler, 'rosetta/pds_handler'
-      autoload :ProducerHandler, 'rosetta/producer_handler'
-      autoload :DepositHandler, 'rosetta/deposit_handler'
-      autoload :SipHandler, 'rosetta/sip_handler'
-      autoload :IeHandler, 'rosetta/ie_handler'
-      autoload :CollectionHandler, 'rosetta/collection_handler'
-      autoload :OaiPmh, 'rosetta/oai_pmh'
+      autoload :PdsHandler, 'libis/services/rosetta/pds_handler'
+      autoload :ProducerHandler, 'libis/services/rosetta/producer_handler'
+      autoload :DepositHandler, 'libis/services/rosetta/deposit_handler'
+      autoload :SipHandler, 'libis/services/rosetta/sip_handler'
+      autoload :IeHandler, 'libis/services/rosetta/ie_handler'
+      autoload :CollectionHandler, 'libis/services/rosetta/collection_handler'
+      autoload :OaiPmh, 'libis/services/rosetta/oai_pmh'
     end
   end
 end
